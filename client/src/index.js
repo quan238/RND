cd857be386b7411b2a8bf7a60deeb0d99db1e5fa
quantran238 <quan.tran@southasiaservices.com>
2023-01-02 00:25:53 +0700

    fix: cors

--- client/src/index.js
+++ client/src/index.js
@@ -9,6 +9,10 @@ import "./config/i18n";
 import { I18nextProvider } from "react-i18next";
 import i18next from "i18next";
 
+import axios from "axios";
+
+axios.defaults.baseURL = process.env.REACT_APP_BE_URL;
+
 ReactDOM.render(
   <I18nextProvider i18n={i18next}>
     <ToastContainer />
