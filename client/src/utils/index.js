c45eda7069b542c0e93f2e0377a74159a20a1ea4
quantran238 <quan.tran@southasiaservices.com>
2023-02-04 11:51:29 +0700

    fix: dashboard

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -53,3 +53,23 @@ export function formatVNDCurrency(amount) {
 export function formatCurrency(amount) {
   return amount.toLocaleString("en-US", { style: "currency", currency: "USD" });
 }
+
+export function stringToColor(string) {
+  let hash = 0;
+  let i;
+
+  /* eslint-disable no-bitwise */
+  for (i = 0; i < string.length; i += 1) {
+    hash = string.charCodeAt(i) + ((hash << 5) - hash);
+  }
+
+  let color = "#";
+
+  for (i = 0; i < 3; i += 1) {
+    const value = (hash >> (i * 8)) & 0xff;
+    color += `00${value.toString(16)}`.substr(-2);
+  }
+  /* eslint-enable no-bitwise */
+
+  return color;
+}
